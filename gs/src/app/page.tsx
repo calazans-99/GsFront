81ac3aefeacd96ff7d3b59fb782c5765d9b268da
calazans-99 <marcuscalazans01@gmail.com>
2024-11-22 21:09:08 -0300

    att cor

--- gs/src/app/page.tsx
+++ gs/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       {/* Header */}
-      <header className="w-full h-[200px] border-t border-primary-dark bg-primary flex justify-center items-center p-4">
+      <header className="w-full h-[200px] border-t border-green-700 bg-green-600 flex justify-center items-center p-4">
         <h1 className="text-6xl text-white text-center celular:text-3xl">
           FlexEnergy
         </h1>
@@ -15,10 +15,10 @@ export default function Home() {
       {/* Seção: Quem somos */}
       <section className="w-full flex justify-evenly items-center gap-3 my-4 md:h-[600px] celular:flex-col-reverse celular:h-max celular:py-4 celular:px-4">
         <div className="w-1/2 flex justify-center items-center flex-col tablet:w-full">
-          <h3 className="text-5xl font-medium pb-16 text-center celular:text-3xl">
+          <h3 className="text-5xl font-medium pb-16 text-center text-green-700 celular:text-3xl">
             Quem nós somos?
           </h3>
-          <p className="text-3xl font-light w-3/4 text-center celular:text-lg">
+          <p className="text-3xl font-light w-3/4 text-center text-green-600 celular:text-lg">
             Nós somos a Flex Energy, uma solução inovadora para promover o uso
             eficiente de energia sustentável. Nosso objetivo é ajudar você a
             monitorar, planejar e reduzir seu impacto ambiental.
@@ -36,7 +36,7 @@ export default function Home() {
       </section>
 
       {/* Seção: Como faremos isso */}
-      <section className="w-full h-[600px] bg-primary flex justify-center items-center flex-col px-4 celular:h-auto celular:py-10">
+      <section className="w-full h-[600px] bg-green-600 flex justify-center items-center flex-col px-4 celular:h-auto celular:py-10">
         <h2 className="text-5xl font-medium text-white pb-[50px] text-center celular:text-3xl">
           Como faremos isso?
         </h2>
@@ -59,10 +59,10 @@ export default function Home() {
           />
         </div>
         <div className="w-3/4 flex justify-center items-center flex-col tablet:w-full celular:w-full">
-          <h2 className="text-5xl font-medium text-center pb-[50px] text-black celular:text-3xl celular:pb-7">
+          <h2 className="text-5xl font-medium text-center pb-[50px] text-green-700 celular:text-3xl celular:pb-7">
             O que mais te oferecemos?
           </h2>
-          <p className="text-3xl font-light text-black text-center w-3/4 celular:text-[20px]">
+          <p className="text-3xl font-light text-green-600 text-center w-3/4 celular:text-[20px]">
             Além de monitoramento e diagnósticos, oferecemos soluções para
             otimizar seu consumo energético. Fornecemos também um guia de
             implementação de projetos de energia renovável, como painéis solares
